2a96ca1d250f32ecd886214ccc62d3b67c5df5f1
David Ruiz <cavapaslatet@gmail.com>
2016-03-26 16:12:56 -0700

    Initial Commit

---
+++ AideMemoire/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AideMemoire
+//
+//  Created by David Ruiz on 26/03/2016.
+//  Copyright © 2016 davidruizrao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AideMemoire/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AideMemoire
+//
+//  Created by David Ruiz on 26/03/2016.
+//  Copyright © 2016 davidruizrao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
